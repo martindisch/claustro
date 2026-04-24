de4f0ce596c0d1570650f0382e3edd7be64ebde9
Martin Disch <martindisch@gmail.com>
2026-04-24 23:12:10 +0200

    Remove unnecessary handler

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,6 @@ fn main() -> Result<ExitCode> {
     let resolved_mounts = mounts::resolve(&cli.mounts)?;
     let image_tag = cli::derive_image_tag(&cli.image)?;
 
-    // Best-effort to attempt to prevent Ctrl-C from killing the process before
-    // we've cleaned up the temp dir
-    ctrlc::set_handler(|| {})?;
-
     docker::build(&cli.image, &image_tag)?;
 
     // Mounted temporary directory with a copy of the host credentials, so that
